afe517b56f390de871ae4162337bf615aa007dd6
OthmaneB-Pro <othmane.baar@gmail.com>
2025-08-19 19:00:38 +0200

    Create project

---
+++ src/main/java/bookly_backend/BooklyBackendApplication.java
@@ -0,0 +1,13 @@
+package bookly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklyBackendApplication.class, args);
+	}
+
+}
